83ffbee808a85054cfc1c49861f8a326f1dedbe1
Konstantin8105 <konstantin8105@gmail.com>
2023-09-29 11:56:55 +0300

    init

---
+++ go.mod
@@ -0,0 +1,26 @@
+module github.com/Konstantin8105/smoke
+
+go 1.20
+
+require (
+	github.com/Konstantin8105/ds v0.0.0-20230911101041-2823ea6497ce
+	github.com/Konstantin8105/ms v0.0.0-20230926174444-e6b5afed6740
+	github.com/Konstantin8105/vl v0.0.0-20230923113629-0d20b8dc5a5d
+)
+
+require (
+	github.com/Konstantin8105/glsymbol v0.0.0-20230929084058-89782ab16f60 // indirect
+	github.com/Konstantin8105/tf v0.0.0-20230612182433-2ec4a099d755 // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.6.0 // indirect
+	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20221017161538-93cebf72946b // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	golang.org/x/image v0.12.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/term v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+)
---
+++ timer.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/Konstantin8105/ds"
+	"github.com/Konstantin8105/glsymbol"
+	"github.com/Konstantin8105/ms/window"
+	"github.com/Konstantin8105/vl"
+)
+
+func main() {
+	ch := make(chan func() (fus bool), 1000)
+
+	var design vl.Widget
+	{
+		var list vl.List
+
+		list.Add(new(vl.Separator))
+
+		var status vl.Text
+
+		var limit int = 5400 // 1.5 hours
+		var timer vl.Text
+		start := time.Now()
+		go func() {
+			for {
+				ch <- func() (fus bool) {
+					diff := time.Now().Sub(start)
+					sec := int(diff.Seconds())
+					if sec < limit {
+						status.SetText("")
+					} else {
+						status.SetText("TIME IS OVER")
+					}
+					timer.SetText(fmt.Sprintf("%06d", sec))
+					return true
+				}
+				time.Sleep(time.Millisecond * 500)
+			}
+		}()
+		list.Add(&timer)
+		list.Add(new(vl.Separator))
+
+		list.Add(&status)
+		list.Add(new(vl.Separator))
+
+		var reset vl.Button
+		reset.SetText("Reset")
+		reset.OnClick = func() {
+			start = time.Now()
+		}
+		list.Add(&reset)
+
+		list.Add(new(vl.Separator))
+
+		step := int64(300) // seconds
+		var add vl.Button
+		add.SetText(fmt.Sprintf("Add %d sec", step))
+		add.OnClick = func() {
+			dur := time.Duration(-step * 1e9)
+			start = start.Add(dur)
+		}
+		list.Add(&add)
+
+
+
+		design = &list
+	}
+
+	var doc vl.Widget
+	doc = vl.TextStatic(`Documentation:
+1) Reset timer to zero.
+2) Add on step seconds.
+3) After 5400 seconds indicated`)
+
+	var (
+		left  = window.NewTui(design)
+		right = window.NewTui(doc)
+
+		ws = [2]ds.Window{left, right}
+	)
+
+	screen, err := ds.New("Timer", ws, &ch)
+	if err != nil {
+		panic(err)
+	}
+
+	// add fonts
+	f, err := DefaultFont()
+	if err != nil {
+		return
+	}
+	left.SetFont(f)
+	right.SetFont(f)
+
+	quit := make(chan struct{})
+
+	screen.Run(&quit)
+}
+
+// DefaultFont return default font
+func DefaultFont() (_ *glsymbol.Font, err error) {
+	var (
+		low   = rune(byte(32))
+		high  = rune(byte(127))
+		scale = int32(32) // font size
+	)
+	return glsymbol.LoadTruetype(
+		strings.NewReader(glsymbol.DefaultEmbeddedFont),
+		scale,
+		rune(byte(low)),
+		rune(byte(high)),
+	)
+}
